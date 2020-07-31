afd44999eea3ddf4fad9cfd31cde5baa74e4a780
wglebster <wglebster@gmail.com>
2020-07-31 22:53:12 +0100

    commit task 4 of the lab

--- Week_05/Day_4/lab/app_with_tabs/app.R
+++ Week_05/Day_4/lab/app_with_tabs/app.R
@@ -1,3 +1,66 @@
 library(CodeClanData)
 library(tidyverse)
+library(shiny)
 
+# UI section
+ui <- fluidPage(
+    
+    titlePanel("Comparing Importance of Internet Access vs. Reducing Pollution"),
+    
+    sidebarLayout(
+        sidebarPanel(
+            
+            selectInput("gender_select",
+                        "Gender",
+                        choices = c("Male", "Female")
+                        ),
+            selectInput("region_select", 
+                        "Region", 
+                        choices = sort(unique(students_big$region))
+                        ), #remove students_big later
+            actionButton("update_all",
+                         "Generate Plots and Table")
+        ),
+        mainPanel(
+            tabsetPanel(
+                tabPanel("Plots",
+                         column(6,
+                         plotOutput("importance_internet_access"),#plot1
+                         ),
+                         column(6,
+                         plotOutput("importance_reducing_pollution")#plot2
+                         )
+                         ),
+                tabPanel("Table",
+                         tableOutput("filtered_table")
+                         ) #table
+           )
+        )
+    )
+)
+# Server section
+server <- function(input, output) {
+    filtered_data <- eventReactive(input$update_all, {
+        students_big %>%
+            filter(gender <= input$gender_select) %>%
+            filter(region <= (input$region_select))
+    })
+    
+    output$importance_internet_access <- renderPlot({
+        ggplot(filtered_data()) + 
+            aes(x = importance_internet_access) + 
+            geom_histogram()
+    })
+    
+    output$importance_reducing_pollution <- renderPlot({
+        ggplot(filtered_data()) + 
+            aes(importance_reducing_pollution) + 
+            geom_histogram()
+    })
+ 
+    output$filtered_table <- renderTable(
+        filtered_data()
+    )
+}
+# Run the app
+shinyApp(ui = ui, server = server)
