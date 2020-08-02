51907118c99e639f68dc1301bb31168d65fe346f
wglebster <wglebster@gmail.com>
2020-08-02 11:08:02 +0100

    add second tab

--- Week_05/Weekend_homework/shiny_app/server.R
+++ Week_05/Weekend_homework/shiny_app/server.R
@@ -1,33 +1,32 @@
 #server.R
-server <- function(input, output) {
-  filtered_data <- reactive({
+server <- function(input, output){
+  filtered_data <- reactive( {
     game_sales %>%
-      filter(year_of_release == input$year_select)
-    
-      # filter(genre == input$genre_select) %>%
-      # filter(platform == input$platform_select) %>%
-      # filter(rating == input$rating_select) %>%
+      filter(year_of_release == input$year_select) %>%
+      filter(genre == input$genre_select) %>%
+      filter(platform == input$platform_select) %>%
+      filter(rating == input$rating_select)
       
-  })
-  
-  output$filtered_table <- renderDataTable(
-    if(input$genre_select == "all") {
-      return(filtered_data())
-    } else if(input$platform_select == "all") {
-      return(filtered_data())
-    } else if (input$rating_select == "all") {
-      return(filterred_data())
-      
-    }
-    # filter(genre == input$genre_select)
-    # if(input$platform_select == "all") {
-    #   return(filtered_data())
-    # } else { 
-    #   filtered_data() %>%
-    #     filter(platform == input$platform_select)
+    # 
+    # if(input$genre_select != "all") {
+    #   (filtered_data() %>% 
+    #      filter(genre == input$genre_select))
+    # } else if(input$platform_select != "all") {
+    #   (filtered_data() %>%
+    #      filter(platform == input$platform_select))
+    # } else if (input$rating_select != "all") {
+    #   (filterred_data() %>% 
+    #      filter(rating == input$rating_select))
+    # } else {
+    #   filter(year_of_release == input$year_select)
     # }
     
-    # filtered_data(), 
-    # options = list(searching = FALSE, paging = TRUE)
-  )
+    })
+    output$filtered_table <- renderDataTable({
+      filtered_data() 
+      options = list(searching = FALSE, paging = TRUE)
+  })
+    output$popularity_plot <- renderPlot({
+      
+    })
 }
--- Week_05/Weekend_homework/shiny_app/ui.R
+++ Week_05/Weekend_homework/shiny_app/ui.R
@@ -1,4 +1,3 @@
-#ui.R
 ui <- fluidPage(
   titlePanel(tags$h1("Gamer's Paradise",
                      tags$h5("Thank you for using Gamer's Paradise dashboard,
@@ -6,45 +5,63 @@ ui <- fluidPage(
                      we have in our database.
                      We hope this Shiny dashboard will make it easier for you to
                      choose your perfect game!"))
-             ),
-  sidebarLayout(
-    sidebarPanel(width = 4,
+  ),
+  
+  tabsetPanel(
+    tabPanel("Games Filter",
+             sidebarLayout(
+               sidebarPanel(
                  sliderInput("year_select",
-                             "Select years of release",
-                             min = min_slider,
-                             max = max_slider,
-                             value = c(min_slider, max_slider),
-                             sep = "",
-                             ticks = FALSE  
-                            ),
+                  "Select years of release",
+                  min = min_slider,
+                  max = max_slider,
+                  value = c(min_slider, max_slider),
+                  sep = "",
+                  ticks = FALSE
+                  ),
                  selectInput("genre_select",
-                             "Select genre", 
-                             choices = genre_choice,
-                             selected = "all"
-                             ),
+                  "Select genre", 
+                  choices = genre_choice,
+                  #selected = "all"
+                  ),
                  selectInput("platform_select",
-                             "Select your platform",
-                             choices = platform_choice,
-                             selected = "all"
-                             ),
+                  "Select your platform",
+                  choices = platform_choice,
+                  #selected = "all"
+                  ),
                  radioButtons("rating_select",
-                              "Select age rating",
-                              choices = rating_choice,
-                              inline = TRUE, 
-                              selected = "all"
-                              ),
+                   "Select age rating",
+                   choices = rating_choice,
+                   inline = TRUE, 
+                   #selected = "all"
+                   ),
                  #actionButton("update_all",
-                  #            "Show me my Games!")
-                 ),
+                 #"Show me my Games!")
+    ),
     
     mainPanel(
-      tabsetPanel(
-        tabPanel("Your list of Games",
-                 dataTableOutput("filtered_table")
-                 ),
-        tabPanel("Some visual Stats"
-                 )
-      )
+      dataTableOutput("filtered_table")
     )
+      
+      )
+    ),
+    tabPanel("Interesting Stats",
+             sidebarLayout(
+               sidebarPanel(
+                 radioButtons("stats_selection",
+                             "Choose your stats",
+                             choices = c("Top 10 critic rated", 
+                                         "Top 10 user rated", 
+                                         "Top 10 sales", 
+                                         "Compare all")
+                             )
+                 
+               ),
+               mainPanel(
+                 plotOutput("popularity_plot")
+               )
+             )
+            )
   )
 )
+
