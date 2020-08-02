e7ae89abcf980a4fb8a43adcfbe8f4131b694ae6
wglebster <wglebster@gmail.com>
2020-08-02 09:12:23 +0100

    try render with if else if

--- Week_05/Weekend_homework/shiny_app/server.R
+++ Week_05/Weekend_homework/shiny_app/server.R
@@ -2,19 +2,32 @@
 server <- function(input, output) {
   filtered_data <- reactive({
     game_sales %>%
-      filter(year_of_release == input$year_select) %>%
-      filter(genre == input$genre_select) %>%
-      filter(platform == input$platform_select) %>%
-      filter(rating == input$rating_select)
+      filter(year_of_release == input$year_select)
+    
+      # filter(genre == input$genre_select) %>%
+      # filter(platform == input$platform_select) %>%
+      # filter(rating == input$rating_select) %>%
+      
   })
   
   output$filtered_table <- renderDataTable(
-  
-    filtered_data(), 
-    options = list(searching = FALSE, paging = TRUE)
+    if(input$genre_select == "all") {
+      return(filtered_data())
+    } else if(input$platform_select == "all") {
+      return(filtered_data())
+    } else if (input$rating_select == "all") {
+      return(filterred_data())
+      
+    }
+    # filter(genre == input$genre_select)
+    # if(input$platform_select == "all") {
+    #   return(filtered_data())
+    # } else { 
+    #   filtered_data() %>%
+    #     filter(platform == input$platform_select)
+    # }
     
-    # filter(genre == input$genre_select) %>%
-    #   filter(platform == input$platform_select) %>%
-    #   filter(rating == input$rating_select)
+    # filtered_data(), 
+    # options = list(searching = FALSE, paging = TRUE)
   )
 }
